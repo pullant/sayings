2b8c790a79772c90c9d26d930575d85ed64bcdff
pullant <108009450+pullant@users.noreply.github.com>
2024-01-12 02:46:04 -0300

    Add change phrase functionality: change_phrase()

--- change.py
+++ change.py
@@ -1,13 +1,8 @@
 import csv
-from datetime import datetime, timedelta
 from shutil import copy
 
-from build_queue import build_queue
-from delete import delete_entry
-from change import change_phrase
 import config
 
-INFO = config.INFO
 QUEUE = config.QUEUE
 PHRASES = config.PHRASES
 queue_temp = config.queue_temp
@@ -34,41 +29,29 @@ def main():
 
 def review_phrase():
 
-    # CHECK if QUEUE exist
-    # if it does not exist return home "Nothing to review."
     if not QUEUE.exists():
         print('\n' + 'Nothing to review.')
         return
 
-    # SET review mode
     while True:
 
-        # CALL QUEUE builder
         today = build_queue()
 
-        # GET first entry from QUEUE
         queue = QUEUE.open('r', newline='')
         reader = csv.DictReader(queue)
         for entry in reader:
             print('\n' + '  “ ' + entry['phrase'] + ' ”')
             break
-            # break out of the loop because if it finishes,
-            # it will execute the else statement
-        # if QUEUE is empty return home
         else:
             print('\n' + 'Review done!')
             queue.close()
-            return # go home
-            # if there is no row the loop will finish
-            # and the else statement will be executed
+            return
 
-        # ASK the user to qualify the phrase
         response = input("""
 ╭────────┬────────┬─────────┬──────────┬──────────┬────────╮
 │ [e]asy │ [h]ard │ [a]gain │ [c]hange │ [d]elete │ [q]uit │
 ├────────┴────────┴─────────┴──────────┴──────────┴────────╯
 ╰──> """)
-        # update entry according to user input or quit
         if response == "e":
             easy_phrase(entry, today)
         elif response == "h":
@@ -83,36 +66,13 @@ def review_phrase():
             break
 
     queue.close()
-    return # go home
+    return
 
 
-def easy_phrase(entry, today):
+def change_phrase(entry, today):
 
-    # days for easy = days added last time * 2
-    days_to_next_review = int(entry['days']) * 2
-
-    update_entry(entry, days_to_next_review, today)
-
-
-def hard_phrase(entry, today):
-
-    # days for hard = days for easy / 2 = days added last time
-    days_to_next_review = int(entry['days'])
-
-    update_entry(entry, days_to_next_review, today)
-
-
-def update_entry(entry, days_to_next_review, today):
-
-    # GET new date for entry
-    # entry['date'] = today
-    # - not reviewed older entries in the queue are going to be reviewed
-    # as if they were added today
-    # - so the next review is going to be scheduled counting from today
-
-    # new date = date + days to next review
-    new_date = datetime.strptime(today, '%Y%m%d') + timedelta(days=days_to_next_review)
-    new_date = new_date.strftime('%Y%m%d')
+    # get new phrase
+    phrase = input('\nphrase // ')
 
     # UPDATE entry in PHRASES
         # read phrases
@@ -127,15 +87,12 @@ def update_entry(entry, days_to_next_review, today):
             writer.writeheader()
             for row in reader:
                 if row['ID'] == entry['ID']:
-                    row['days'] = days_to_next_review
-                    row['date'] = new_date
+                    row['phrase'] = phrase
                     writer.writerow(row)
                 else:
                     writer.writerow(row)
     copy(phrases_temp, PHRASES)
 
-    # REMOVE entry from QUEUE
-    # remove top entry in QUEUE
     with QUEUE.open('r', newline='') as queue:
         reader = csv.DictReader(queue, fieldnames=fieldnames)
         with queue_temp.open('w', newline='') as temp:
@@ -145,41 +102,23 @@ def update_entry(entry, days_to_next_review, today):
             writer.writeheader()
             for row in reader:
                 writer.writerow(row)
+            writer.writerow({'ID': entry['ID'], 'date': entry['date'], 'days': entry['days'], 'phrase': phrase})
     copy(queue_temp, QUEUE)
+
+
+def easy_phrase(entry, today):
+    return
+
+
+def hard_phrase(entry, today):
+    return
 
 
 def again_phrase(entry, today):
+    return
 
-    # MOVE entry to the end of QUEUE
-    with QUEUE.open('r', newline='') as queue:
-        reader = csv.DictReader(queue, fieldnames=fieldnames)
-        with queue_temp.open('w', newline='') as temp:
-            writer = csv.DictWriter(temp, fieldnames=fieldnames)
-
-            # read QUEUE from the second row forward
-            next(queue) # skip header
-            next(queue) # skip first row in QUEUE
-
-            # write it to queue_temp
-            writer.writeheader()
-            for row in reader:
-                writer.writerow(row)
-
-                # if the reader is used
-                # - after the stream was moved (next(queue))
-                # - and no fieldnames were given
-                # the reader takes the data in the new position as
-                # the key values for the dictionary been created
-                # solution:
-                # - add the fieldnames to the reader
-                # - and then move stream as necessary
-
-            # update date of entry and write it to the end
-            entry['date'] = today
-            writer.writerow(entry)
-
-    copy(queue_temp, QUEUE)
-
+def delete_entry(entry, today):
+    return
 
 def write_phrase():
     return
